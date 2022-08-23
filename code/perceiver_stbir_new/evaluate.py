33d74bba3088be4321b955375a38d91898391ca1
ramyapriya <ramyapriya199607@gmail.com>
2022-08-24 00:18:56 +0100

    changess

--- code/perceiver_stbir_new/evaluate.py
+++ code/perceiver_stbir_new/evaluate.py
@@ -30,47 +30,19 @@ def validation_step(self, val_batch, batch_idx):
     
     
 if __name__ == "__main__":
+    ckpt_path = '/vol/research/sketchcaption/ramya/dissertation_exps/training_runs/logs_july14th_v1/saved_model/sketchycoco-epoch=190-top10=0.67.ckpt'
     dataset_transforms = transforms.Compose([
         transforms.Resize((opts.max_len, opts.max_len)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    val_dataset = CustomSketchyCOCO(opts, mode='val',
-        transform=dataset_transforms)
+    val_dataset = CustomSketchyCOCO(opts, mode='val', transform=dataset_transforms, debug=False)
 
     val_loader = DataLoader(
         dataset=val_dataset, batch_size=opts.batch_size, num_workers=opts.workers)
     print('Val DataLoader loaded with %d items' % len(val_dataset))
-
-    ckpt_path = '/vol/research/sketchcaption/ramya/dissertation_exps/training_runs/logs_july14th_v1/saved_model/sketchycoco-epoch=190-top10=0.67.ckpt'
+    
     model = TripletNetwork.load_from_checkpoint(ckpt_path)
     trainer = Trainer(gpus=1)
-    trainer.validate(model, val_loader)
-
-    query_feature_all_tensor, img_feature_all_tensor = [], []
-    for idx, (txt_tensor, txt_length, sk_tensor, img_tensor, neg_tensor,
-            text_data, text_emb, sketch_data, image_data, negative_data) in enumerate(val_dataset):
-        import ipdb; ipdb.set_trace()
-        txt_feature = model.txt_embedding_network(txt_tensor, txt_length)
-        sk_feature = model.img_embedding_network(sk_tensor)
-        img_feature = model.img_embedding_network(img_tensor)
-        neg_feature = model.img_embedding_network(neg_tensor)
-        query_feature = model.setattn_network(sk_feature, txt_feature)
-        query_feature_all_tensor.append(query_feature)
-        img_feature_all_tensor.append(img_feature)
-        
-    Len = len(query_feature_all_tensor)
-    query_feature_all = torch.cat([query_feature_all[i]for i in range(Len)])
-    image_feature_all = torch.cat([query_feature_all[i]for i in range(Len)])
-
-    rank = torch.zeros(len(query_feature_all))
-    for idx, query_feature in enumerate(query_feature_all):
-        distance = F.pairwise_distance(query_feature.unsqueeze(0), image_feature_all)
-        target_distance = F.pairwise_distance(
-            query_feature.unsqueeze(0), image_feature_all[idx].unsqueeze(0))
-        rank[idx] = distance.le(target_distance).sum()
-
-    print('top1', rank.le(1).sum().numpy() / rank.shape[0])
-    print('top10', rank.le(10).sum().numpy() / rank.shape[0])
-    print('meanK', rank.mean().numpy().item())
+    trainer.test(model, val_loader)
--- code/perceiver_stbir_new/model.py
+++ code/perceiver_stbir_new/model.py
@@ -26,7 +26,7 @@ class TripletNetwork(pl.LightningModule):
         feature = self.embedding_network(x)
         return feature
 
-    def configure_optimizers(self, lr=1e-2):
+    def configure_optimizers(self, lr=1e-4):
         optimizer = optim.Lamb(self.parameters(), lr=lr)
         return optimizer
 
@@ -53,8 +53,37 @@ class TripletNetwork(pl.LightningModule):
         loss = self.loss(query_feature, img_feature, neg_feature)
         self.log('val_loss', loss, prog_bar=True)
         return query_feature, img_feature
+    
+    def test_step(self, val_batch, batch_idx):
+        # defines the validation loop
+        txt_tensor, txt_length, sk_tensor, img_tensor, neg_tensor = val_batch
+        txt_feature = self.txt_embedding_network(txt_tensor, txt_length)
+        import ipdb; ipdb.set_trace()
+        sk_feature = self.img_embedding_network(sk_tensor)
+        img_feature = self.img_embedding_network(img_tensor)
+        neg_feature = self.img_embedding_network(neg_tensor)
+        query_feature = self.setattn_network(sk_feature, txt_feature)
+        loss = self.loss(query_feature, img_feature, neg_feature)
+        self.log('val_loss', loss, prog_bar=True)
+        return query_feature, img_feature
 
     def validation_epoch_end(self, validation_step_outputs):
+        Len = len(validation_step_outputs)
+        query_feature_all = torch.cat([validation_step_outputs[i][0] for i in range(Len)])
+        image_feature_all = torch.cat([validation_step_outputs[i][1] for i in range(Len)])
+
+        rank = torch.zeros(len(query_feature_all))
+        for idx, query_feature in enumerate(query_feature_all):
+            distance = F.pairwise_distance(query_feature.unsqueeze(0), image_feature_all)
+            target_distance = F.pairwise_distance(
+                query_feature.unsqueeze(0), image_feature_all[idx].unsqueeze(0))
+            rank[idx] = distance.le(target_distance).sum()
+
+        self.log('top1', rank.le(1).sum().numpy() / rank.shape[0], prog_bar=True)
+        self.log('top10', rank.le(10).sum().numpy() / rank.shape[0], prog_bar=True)
+        self.log('meanK', rank.mean().numpy().item(), prog_bar=True)
+        
+    def test_epoch_end(self, validation_step_outputs):
         Len = len(validation_step_outputs)
         query_feature_all = torch.cat([validation_step_outputs[i][0] for i in range(Len)])
         image_feature_all = torch.cat([validation_step_outputs[i][1] for i in range(Len)])
